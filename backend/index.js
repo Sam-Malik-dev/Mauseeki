bdb17648f879ee8d3d7440d63dc183a2f28f2321
Sam-Malik-dev <sameerimran967@gmail.com>
2026-09-23 19:24:20 +0500

    Render Fix

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,35 @@
-const express = require("express");
-require("dotenv").config();
-require("./Models/DB");
-
-const cors = require("cors");
-
-const Userrouter = require("./Routers/UserRouters");
-const Songrouter = require("./Routers/SongsRouter");
-const Artistrouter = require("./Routers/ArtistRouter");
-const AlbumRouter = require("./Routers/AlbumRouter");
-const LikeRouter = require("./Routers/LikeRouter");
-const FavRouter = require("./Routers/FavRouter");
-const FollowRouter = require("./Routers/FollowRouter");
-const AdminRouter = require("./Routers/AdminRouter");
-const ContactRouter = require("./Routers/ContactRouter");
-const router = require("./Routers/SearchRouter");
-const recentRouter = require("./Routers/RecentRouter");
+const express = require('express');
+require('dotenv').config();
+require('./Models/DB');
+const cors = require('cors');
+const Userrouter = require('./Routers/UserRouters');
+const Songrouter = require('./Routers/SongsRouter');
+const Artistrouter = require('./Routers/ArtistRouter');
+const AlbumRouter = require('./Routers/AlbumRouter');
+const likeRouter = require('./Routers/LikeRouter');
+const FavRouter = require('./Routers/FavRouter');
+const FollowRouter = require('./Routers/FollowRouter');
+const AdminRouter = require('./Routers/AdminRouter');
+const ContactRouter = require('./Routers/ContactRouter');
+const router = require('./Routers/SearchRouter');
+const recentRouter = require('./Routers/RecentRouter');
 const PlaylistRouter = require("./Routers/PlaylistRouter");
-
 const app = express();
-
-
-// Middleware
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
-
-
-// Routes
-app.use("/Mauseeki", Userrouter);
-app.use("/Mauseeki", Songrouter);
-app.use("/Mauseeki", Artistrouter);
-app.use("/Mauseeki", AlbumRouter);
-app.use("/Mauseeki", LikeRouter);
-app.use("/Mauseeki", FavRouter);
-app.use("/Mauseeki", FollowRouter);
-app.use("/Mauseeki", AdminRouter);
-app.use("/Mauseeki", ContactRouter);
-app.use("/Mauseeki", router);
-app.use("/Mauseeki", recentRouter);
+app.use('/Mauseeki', Userrouter);
+app.use('/Mauseeki', Songrouter);
+app.use('/Mauseeki', Artistrouter);
+app.use('/Mauseeki', AlbumRouter);
+app.use('/Mauseeki', likeRouter);
+app.use('/Mauseeki', FavRouter);
+app.use('/Mauseeki', FollowRouter);
+app.use('/Mauseeki', AdminRouter);
+app.use('/Mauseeki', ContactRouter);
+app.use('/Mauseeki', router);
+app.use('/Mauseeki', recentRouter);
 app.use("/Mauseeki", PlaylistRouter);
-
-
-app.get("/", (req, res) => {
-    res.send("Mauseeki Backend API is Running");
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
 });
-
-
-// FOR VERCEL
-module.exports = app;
